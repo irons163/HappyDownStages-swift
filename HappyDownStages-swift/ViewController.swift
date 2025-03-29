48b006580f04b3a75ba5f01a454aa650a729b2bb
Phil <irons7654321@gmail.com>
2025-03-29 21:01:39 +0800

    Initial Commit

---
+++ HappyDownStages-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HappyDownStages-swift
+//
+//  Created by Phil on 2025/3/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HappyDownStages-swiftTests/HappyDownStages_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  HappyDownStages_swiftTests.swift
+//  HappyDownStages-swiftTests
+//
+//  Created by Phil on 2025/3/29.
+//
+
+import Testing
+@testable import HappyDownStages_swift
+
+struct HappyDownStages_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
